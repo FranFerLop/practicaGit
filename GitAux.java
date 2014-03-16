6cb0d0fbc1cdf1c8a3df5d172b89029041986a62
JesusGalera <gsuscullar@gmail.com>
2014-03-16 17:54:17 +0100

    modificaciones alumno 2

--- GitAux.java
+++ GitAux.java
@@ -6,7 +6,7 @@ package prgitejemplo;
 
 /**
  *
- * @author Francisco
+ * @author Francisco and Jesus
  */
 public class GitAux {
     
@@ -14,10 +14,9 @@ public class GitAux {
         System.out.println("metodo 1");
     }
     
-    public void metodoAlumno2(){
-	System.out.println("metodo 2");
-    }
-    
+   public void metodoAlumno2(){ 
+ System.out.println("metodo 2"); 
+ } 
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
     }
