21662c589ee3b7348d33f84e61c2e348c68da124
Francisco <franferlop93@gmail.com>
2014-03-16 19:16:42 +0100

    Actualizacion Alumno 1

--- GitAux.java
+++ GitAux.java
@@ -12,6 +12,7 @@ public class GitAux {
     
     public void metodoAlumno1(){
         System.out.println("metodo 1");
+	System.out.println("modificado por alumno 1"); // alumno 1
     }
     
     public void metodoAlumno2(){
