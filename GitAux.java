aaac49f15d59e3886480afa6e7e9e959d0590e6c
JesusGalera <gsuscullar@gmail.com>
2014-03-16 19:39:06 +0100

    Modificaciones Jesus Galera

--- GitAux.java
+++ GitAux.java
@@ -12,7 +12,7 @@ public class GitAux {
     
     public void metodoAlumno1(){
         System.out.println("metodo 1");
-	System.out.println("modificado por alumno 1"); // alumno 1
+	
     }
     
    public void metodoAlumno2(){ 
@@ -22,8 +22,7 @@ public class GitAux {
  } 
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
- 	// modificaciones alumno 1
-	System.out.println("Alumno 1 inserta esta sentencia");
+
 // modificaciones Jesus Galera
  System.out.println("Jesus Galera inserta esta sentencia"); 
     }
